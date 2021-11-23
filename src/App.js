0ce219365a299aa176b84692dfc24ac9d010aad8
Jose Marti Rios <josemartzrios14@gmail.com>
2021-11-22 18:47:47 -0600

    imgs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [number, setNumber] = useState(0)
+
+
+  const masUno = () => {
+    setNumber(number + 1);
+  }
+
+  const menosUno = () => {
+    setNumber(number - 1);
+  }
+
+  
+
+  return <>
+      <button onClick={masUno}>+1</button>
+      <button onClick={menosUno}>-1</button>
+
+      <span>{number}</span>
+  
+  </>
+  
 }
 
 export default App;
