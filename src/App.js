3150e36963970ca93e329fd7681cf137862024d5
Jose Marti Rios <josemartzrios14@gmail.com>
2021-11-25 13:10:20 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-
+import html2canvas from 'html2canvas';
 import './App.css';
 import React, {useState} from 'react';
 
 function App() {
 
-  const [number, setNumber] = useState(0)
+  const [lineaUno, setLineaUno] = useState('');
+  const [lineaDos, setLineaDos] = useState('');
+  const [imagen, setImagen] = useState('')
 
-
-  const masUno = () => {
-    setNumber(number + 1);
+  const onChangeEvent1 = (e) => {
+    setLineaUno(e.target.value);
+    
   }
 
-  const menosUno = () => {
-    setNumber(number - 1);
+  const onChangeEvent2 = (e) => {
+    setLineaDos(e.target.value);
   }
 
+  const onChangeSelect = (evento) => {
+    setImagen(evento.target.value);
+  }
   
+  const functionExportar = (e) => {
+
+      html2canvas(document.querySelector("#meme")).then(canvas => {
+      // converti en canvas mi imagen
+      // Ya que hice eso descargo el canvas a mi dispositivo 
+      var img = canvas.toDataURL('image/png');
+      
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click()
+    });
+  }
 
   return <>
-      <button onClick={masUno}>+1</button>
-      <button onClick={menosUno}>-1</button>
+    <div className='App'>
 
-      <span>{number}</span>
-  
+      <select onChange={onChangeSelect}>
+        <option value='Dicaprio'>Dicaprio</option>
+        <option value='Futurama'>Futurama</option>
+        <option value='Wonka'>Wonka</option>
+      </select> <br/><br/>
+
+      <input type='text' placeholder='Primera línea'  onChange={onChangeEvent1}/><br/>
+      <input type='text' placeholder='Segunda línea'  onChange={onChangeEvent2}/><br/><br/>
+
+    <button onClick={functionExportar}>Descargar</button> 
+
+    </div>
+
+    <div className='imgDiv' id='meme'>
+      <span>{lineaUno}</span>
+      <span>{lineaDos}</span>
+      {/* para seleccionar la imagen del meme */}
+      <img src={"/img/" + imagen + ".jpg"} />
+    </div>
   </>
   
 }
